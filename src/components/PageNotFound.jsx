175a4e9f80f77522dc90d0d82492a6c66a77508e
rafagomez92 <rafael_gomez92@hotmail.com>
2020-02-20 23:51:20 -0600

    Estos son los jsx para mostrar cuando la pagina no fue encontrada

---
+++ src/components/PageNotFound.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const PageNotFound = () => {
+    return(
+        <div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <div>
+                <h1 className="display-1">¡Oh no!</h1>
+                <h2 className="display-4">La página no se ha encontrado (Error 404)</h2>
+                <h2>Desafortunadamente la página que buscas no existe.</h2>                                
+                <Link className="btn btn-dark" to="/"><h3>Home</h3></Link>                                                            
+            </div>
+        </div>
+    </div>
+</div>
+    );
+}
+
+export default PageNotFound;
